b492c9edc514fd22d2f34cd85de61805b08b5334
halva908 <aminahs2023@mail.ru>
2025-11-26 21:57:25 +0300

    Add files via upload

--- дз3.cpp
+++ дз3.cpp
@@ -1,48 +1,20 @@
-#include <iostream>
+#include <iostream>>
 using namespace std;
 
-int main() {
+int main()
+{
+	int number;
 	setlocale(LC_ALL, "rus");
-	int arr[3][6] = {
-		{10, 25, 3, 18, 7, 30},
-		{14, 5, 22, 9, 16, 4},
-		{27, 8, 19, 2, 21, 11}
-	};
 
-	
-	cout << "Массив:" << endl;
-	for (int i = 0; i < 3; ++i) {
-		for (int j = 0; j < 6; ++j) {
-			cout << arr[i][j] << "\t";  
-		}
-		cout << endl;  
+
+	cout << "Введите число: " << endl;
+	cin >> number;
+	for (int i = 1; i <= 10; ++i)
+	{
+
+		cout << number << "x" << i << "=" << (number * i) << endl;
 	}
-
-	
-	int min_val = arr[0][0];
-	int max_val = arr[0][0];
-	int min_row = 0, min_col = 0;
-	int max_row = 0, max_col = 0;
-
-
-	for (int i = 0; i < 3; ++i) {
-		for (int j = 0; j < 6; ++j) {
-			if (arr[i][j] < min_val) {
-				min_val = arr[i][j];
-				min_row = i;
-				min_col = j;
-			}
-			if (arr[i][j] > max_val) {
-				max_val = arr[i][j];
-				max_row = i;
-				max_col = j;
-			}
-		}
-	}
-
-
-	cout << "Индекс минимального элемента: " << min_row << " " << min_col << endl;
-	cout << "Индекс максимального элемента: " << max_row << " " << max_col << endl;
 
 	return 0;
+
 }
